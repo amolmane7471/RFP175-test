5cc16f887707052020f01950e3081ed14ea3003b
amolmane7471 <maneamol3233@gmail.com>
2022-07-16 09:39:03 +0530

    [Amol]:added Uc2 program changes

--- src/testJava/Uc2.java
+++ src/testJava/Uc2.java
@@ -4,7 +4,7 @@ public class Uc2 {
 	public static void main(String[] args) {
 		System.out.println("you are in main");
 		System.out.println("hello java");
-		System.out.println("hello java 2");
+		System.out.println(" java 2");
 	}
 
 }
